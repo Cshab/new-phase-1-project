2e83b1fbcbe1a22567da85257af1c8ac28ebbd89
Colby Shabel <jcshabel@gmail.com>
2023-04-08 11:40:12 -0500

    added form submit and function

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function initialize(){
     getAllPeople()
 }
 initialize()
+
+// targeting the form and adding a submit with the function of selectedRadioButton to make json objects into arrays and loop through the targeted arrays, calls getDate which pushes them into a li
+document.querySelector('form').addEventListener('submit', selectedRadioButton)
+
+function selectedRadioButton(e){
+    e.preventDefault()
+    let girl = document.getElementById('girl')
+    let boy = document.getElementById('boy')
+    let ul = document.querySelector('#People-list')
+    ul.innerHTML = ''
+    if(girl.checked==true){
+        Object.entries(peopleObj).filter(findGirl => findGirl[1].Gender==='female').forEach((girls,i) => getDate(girls[1],i) )  
+    }
+    else if(boy.checked==true){
+        Object.entries(peopleObj).filter(findBoy => findBoy[1].Gender==='male').forEach((boys,i) => getDate(boys[1],i))
+    }   
+}
